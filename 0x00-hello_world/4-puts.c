18c285de0c8aa0cf762f1a6275eded7e9a8c438c
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-10 14:58:04 -0800

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /*This will display the given text according to the betty code*/
 int main(void)
 {
-       	puts( "Programming is like building a multilingual puzzle\n" );
+       	puts("\"Programming is like building a multilingual puzzle\n" );
        	return(0);
 }
 

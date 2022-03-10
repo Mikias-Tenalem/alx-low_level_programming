bc1dafd16a3a91887780f9125b86e9ba8c76a4fe
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-10 15:20:35 -0800

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main -Main Function
  * Description- printing using puts
- * Return- Returns Void
+ * Return: Returns Void
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
 

885add81e07e2e4eaf100987bc1b02e368b99c3b
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-10 15:35:36 -0800

    tasks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- main function
+ * Description- Prints the task phrase
+ * Return- Returns void
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

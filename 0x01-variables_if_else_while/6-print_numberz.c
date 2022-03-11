5f35df1671ac5506ab6722f9137e0d3a0ddd9659
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-11 15:45:02 -0800

    tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -Main function
+ * Description - Lower Case alphabet letters
+ * Return: Void
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; ++c)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

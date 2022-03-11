aa782d15ab4e9ab468f608bc1befc4adb4d03c46
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-11 15:23:12 -0800

    tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Main function
+ * Description - Lower Case alphabet letters
+ * Return: Void
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+		if(c != q && c != e)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}

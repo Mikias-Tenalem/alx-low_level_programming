ddba346ab6a3c357ee7016cbc73a0246bb813936
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 13:23:50 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,14 @@
 
 /**
  * main - check the code for Holberton School students.
- *
+ * Description - lowercase alphabet printing
  * Return: Always 0.
  */
 void print_alphabet(void)
 {
 	char i;
 
-	for (i ='a'; i <= 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
 	}

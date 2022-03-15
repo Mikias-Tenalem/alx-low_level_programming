2dedd9b70b5ca4528f8948f35a808d99d8c64f32
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 16:37:32 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _islower -Main function
- * Description- affirms lowercase alphabet followed by new line
- * Return: Void
+ * _islower - main function that returns 1 oe 0
+ * @c: takes in a character
+ * Return: 1 for lowercase and 0 for uppercase
  */
 int _islower(int c)
 {

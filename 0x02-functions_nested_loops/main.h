162efd0ab992f1cd0cf3de98acb73aa7bc189598
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 11:28:27 -0700

    Update main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -Main function
+ * Description - prints _putchar followed by new line
+ * Return: Void
+ */
+void putchar_string(char *s)
+{
+	int i = 0;
+	
+	while (s[i] != '\n' && s[i] != '\0')
+	{
+		putchar (s[i]);
+		++i;
+	}
+}

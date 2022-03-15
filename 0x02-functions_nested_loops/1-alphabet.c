d3dc0feb5bdf77b4a0f931d0ac97a13a60ec0d72
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 13:13:31 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+
 /**
- * main -Main function
- * Description - prints _putchar followed by new line
- * Return: Void
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 void print_alphabet(void)
 {
 	char i;
 
 	for (i ='a'; i <= 'z'; i++)
 		putchar(i);
-}
-int main(void)
-{
-	print_alphabet();
-	putchar ('\n');
-	return (0);
 }

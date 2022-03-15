4f72e06de5d2044c7dd3251522b3f67853444abf
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-15 11:40:40 -0700

    tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main -Main function
+ * Description - prints _putchar followed by new line
+ * Return: Void
+ */
+int main(void)
+{
+	char c[] = "abcdefghijklmnopqrstuvwxyz";
+
+	putchar_string(c);
+	putchar ('\n');
+	return (0);
+}

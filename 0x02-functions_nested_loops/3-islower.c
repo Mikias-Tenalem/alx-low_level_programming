5cb41ffbac400f69f007d0b0056a18a8833caac7
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-15 15:49:48 -0700

    tasks

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower -Main function
+ * Description- affirms lowercase alphabet followed by new line
+ * Return: Void
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (islower(c))
+	{
+		putchar('1');
+	}
+	else
+	{
+		putchar('0');
+	}
+}

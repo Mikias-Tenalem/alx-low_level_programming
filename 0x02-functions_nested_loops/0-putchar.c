4819b089fd67b04cd87bc6f73216e2f1de3b989a
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-15 09:43:56 -0700

    tasks

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <>
+/**
+ * main -Main function
+ * Description - prints _putchar followed by new line
+ * Return: Void
+ */
+int main(void)
+{
+	putchar ("_putchar");
+	return (0);
+}

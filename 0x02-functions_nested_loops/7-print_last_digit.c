9b72cc573132bad7f3cf51ce8a3002366572a6a5
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 18:25:59 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,7 +4,6 @@
 /**
  * print_last_digit - main function that returns 1, 0 or -1
  * @n: takes in a character
- * @p: Describes the last digit
  * Description: shows if input is positive, negative or zero
  * Return: 1 for positive, -1 for negative and 0 for 0
  */

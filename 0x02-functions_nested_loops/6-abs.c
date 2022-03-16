82b329412aecc420ae93d6f8eb4807713434bd8d
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 17:59:30 -0700

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,7 +7,7 @@
  * Description: shows if input is positive, negative or zero
  * Return: 1 for positive, -1 for negative and 0 for 0
  */
-int int_abs(int n)
+int _abs(int n)
 {
 	if (n >= 0)
 		return (n);

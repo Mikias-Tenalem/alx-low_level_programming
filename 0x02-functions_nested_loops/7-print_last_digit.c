5c62bc075665701f5c6f8bc2d9cd891386074bd0
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 18:05:01 -0700

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _abs - main function that returns 1, 0 or -1
+ * print_last_digit - main function that returns 1, 0 or -1
  * @n: takes in a character
+ * @p: Describes the last digit
  * Description: shows if input is positive, negative or zero
  * Return: 1 for positive, -1 for negative and 0 for 0
  */
-int _abs(int n)
+int print_last_digit(int n)
 {
-	if (n >= 0)
-		return (n);
-	else
-		return (n * (-1));
-}
+	int p;
+	
+	p = n % 10
+	return (p);
+}	

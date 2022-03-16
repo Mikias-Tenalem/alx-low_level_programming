a4a5875b1893c0ab375e2ee818c7fbf07e4d1d3f
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 18:33:50 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@
  * Description: shows if input is positive, negative or zero
  * Return: 1 for positive, -1 for negative and 0 for 0
  */
-void jack_bauer(int n)
+void jack_bauer(void)
 {
 	int x = 0;
 	int j = 0;

28e5e8dfe1b27f9c17763c89e796004e4c8128e0
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 18:00:44 -0700

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _print_sign - main function that returns 1, 0 or -1
+ * _abs - main function that returns 1, 0 or -1
  * @n: takes in a character
  * Description: shows if input is positive, negative or zero
  * Return: 1 for positive, -1 for negative and 0 for 0

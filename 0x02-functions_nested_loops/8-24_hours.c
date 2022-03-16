99605925659d09f3fc5b76d68d395c4df716171f
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-15 18:35:33 -0700

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,10 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * jack_bauer - main function that returns 1, 0 or -1
- * @n: takes in a character
- * Description: shows if input is positive, negative or zero
- * Return: 1 for positive, -1 for negative and 0 for 0
+ * jack_bauer - tells time
+ * Return: void
  */
 void jack_bauer(void)
 {

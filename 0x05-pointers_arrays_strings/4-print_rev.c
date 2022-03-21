5352604fc3d16bc7119d7ee14dc478fd6c7efb23
Mikias-Tenalem <99313136+Mikias-Tenalem@users.noreply.github.com>
2022-03-21 14:37:56 -0700

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,13 @@ void print_rev(char *s)
 {
 	int i;
 
-	int j;
-
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	}
-	while (j = i && j > -1)
+	while (i > -1)
 	{
-		printf("%c", s[j]);
-		j--;
+		printf("%c", s[i]);
+		i--;
 	}
 	printf("\n");
 }

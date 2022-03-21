05b3e33a766f9d611273235c66e19e64008c07db
Mikias-Tenalem <mikias.tenalem.ayenew@gmail.com>
2022-03-21 13:00:51 -0700

    tasks

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 - Changes value to 98
+ * @n - Variable to be changed
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+	int *p;
+
+	p = &n;
+	*p = 98;
+	return (0);
+}
